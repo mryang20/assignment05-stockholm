9afe59f6be8135b1a0ec22034e42f5e518883540
Irfan Bashir <irfanbashirshah@Github.com>
2018-04-29 18:52:27 -0400

    CSS

--- attractions.php
+++ attractions.php
@@ -20,27 +20,46 @@
 	</header>
 <hr>
 
+<div class = "wrap">
+<img class="pic" src="images/pic.jpg" >
+
 		<h1>Klara kyrka</h1>
 
+
 			<p>Close to the Sergels Torg square, and one of the few buildings in the district to survive the 1960s redevelopment, this large redbrick church was built in the 16th century, following the demolition of a 13th-century nunnery. The 116-metre steeple is the second tallest in Scandinavia and one of the ten tallest building in Sweden, making it a significant landmark. The artwork inside includes an 18th-century altarpiece. In the cemetery, a stone commemorates the 18th-century songwriter Carl Michael Bellman.</p>
+
+</div>
+
+<hr>
+<div class = "wrap">
+<img class="pic" src="images/pic.jpg" >
 
 		<h1>Museum of Modern Art (Moderna Museet)</h1>
 
 			<p>Stockholm's museum of modern art is headed by Lars Nittve, formerly of London's Tate Modern. These museums have several works by famous artists - Pablo Picasso, Henri Mattise, Salvador Dalí and Irving Penn, an American photographer. Although its Stockholm counterpart might not have as vast a collection, there is still enough to satisfy both the modern art buff as well as the curious amateur. Also, the building, by Spanish architect Rafael Moneo, is a sight in itself.</p>
+</div>
+
+<hr>
+<div class = "wrap">
+<img class="pic" src="images/pic.jpg" >
+
 
 		<h1>National Museum (Nationalmuseum)</h1>
 			<p>For those more interested in classical art, Nationalmuseum offers pieces by Rembrandt, Rubens, Goya, Renoir, Degas and Gauguin, as well as well-known Swedish artists such as Carl Larsson, Ernst Josephson, C F Hill and Anders Zorn. The museum also has a collection of applied art, design, and industrial design. The museum is situated in a beautiful 19th century building and has a nice café in its atrium.</p>
+</div>
 
-		<h1>Stockholm Public Library (Stadsbiblioteket)</h1>
+
+<hr>
+<div class = "wrap">
+<img class="pic" src="images/pic.jpg" >
+
+		<h1>Stockholm Public Library </h1>
 
 			<p>Built in 1928 and designed by the famous Swedish architect Erik Gunnar Asplund, the interior of the cylinder-shaped main hall is spectacular, with three floors of bookshelves covering 360 degrees of circular wall. Books (both fiction and non-fiction) are available in many different languages, including English and German. On the cliff overlooking it is the old Observatory, which has a fine view of the city to the east. There is also a small café, and an Observatory Museum.</p>
-<hr>
 
-	<footer>
-		Team Stockholm
-		<br>
-		Assignment 5
-	</footer>
+
+</div>
+	
 
 
 <script src="http://code.jquery.com/jquery.js"></script>
--- restaurants.php
+++ restaurants.php
@@ -17,32 +17,63 @@
 		
 		<?php include "includes/nav.inc" ?>
 	</header>
+
+
+
 <hr>
+<div class = "wrap">
+<img class="pic" src="images/pic.jpg" >
+
 		<h1>Fattoush / Roppongi / Panini</h1>
 
 			<p>A small three-restaurant food court in a central location between the Gallerian shopping center and the NK department store. Fattoush has tasty Lebanese fast food, Roppongi has decent sushi, and Panini offers a selection of sandwiches and salads.</p>
 
+</div>
+
+
+<hr>
+<div class = "wrap">
+<img class="pic" src="images/pic.jpg" >
+
 		<h1>Örtagården</h1>
 			<p>Located on the top floor of the Östermalmshallen food market, Örtagården serves a sumptuous vegetarian buffet with hot and cold vegetarian dishes at a decent price. There is also a "back pocket" serving meat dishes.</p>
+
+</div>
+
+<hr>
+<div class = "wrap">
+<img class="pic" src="images/pic.jpg" >
 
 		<h1>Simons Skafferi No12</h1>
 			<p>Swedish dishes with focus on local and organic ingredients in season, so the menu changes quite often. The employees are very professional and the wine to food pairings are excellent. Open both for lunch and dinner.</p>
 
+</div>
+
+<hr>
+<div class = "wrap">
+<img class="pic" src="images/pic.jpg" >
+
+
 		<h1>Operabaren and Bakfickan</h1>
 
 			<p>Two restaurants in the Royal Opera house, sharing the same menu. Much more laidback, and considerably less expensive than the formal fine dining restaurant Operakällaren and the celebrity-obsessed nightclub Café Opera in the same building. Operabaren and Bakfickan specialise in traditional Swedish cuisine. The rustique "back pocket" Bakfickan is slightly cheaper, but does not allow reservations.</p>
+</div>
+
+<hr>
+<div class = "wrap">
+<img class="pic" src="images/pic.jpg" >
+
 
 		<h1>Café Opera and Operakällaren</h1>
 
-			<p>Situated in the building of the Royal Opera, Café Opera has for long been the place if you want to be seen with celebrities. Offers good food and drinks. Dress code applies. In the same building you'll find a beautiful dining room of the formal and extremely expensive Operakällaren.</p>
+			<p>Situated in the building of the Royal Opera, Café Opera has for long been the place if you want to be seen with celebrities. Offers good food and drinks. Dress code applies. In the same building you'll find a beautiful dining room of the formal and extremely expensive Operakällaren.
 
-<hr>
+</p>
 
-	<footer>
-		Team Stockholm
-		<br>
-		Assignment 5
-	</footer>
+
+
+
+	
 
 <script src="http://code.jquery.com/jquery.js"></script>
 <script src="js/menu-highlighter.js"></script>
