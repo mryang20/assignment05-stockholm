cc82bc55987a951d3c5d599889a9da181f5f554f
Irfan Bashir <irfanbashirshah@Github.com>
2018-04-29 10:23:55 -0400

    update

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 <html lang="en">
 
 <head>
-   
-    <title>Home</title>
+   <meta charset="utf-8">
+
+	<title>Home</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 		
 	
 		<link rel="stylesheet" href="css/styles.css">
