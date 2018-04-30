f72737d838044a55fe8daacee8f54db9b382d79b
cbrentna <35782605+cbrentna@users.noreply.github.com>
2018-04-30 02:41:30 -0400

    login page

--- login.php
+++ login.php
@@ -1,14 +1,85 @@
+<?php
+	require_once 'config.php';
+
+	$username = $password = "";
+	$username_err = $password_err = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+	if(empty(trim($_POST["username"]))){
+        $username_err = 'Please enter username.';
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    if(empty(trim($_POST['password']))){
+        $password_err = 'Please enter your password.';
+    } else{
+        $password = trim($_POST['password']);
+    }
+
+    if(empty($username_err) && empty($password_err)){
+    	$sql = "SELECT username, password FROM users WHERE username = ?";
+
+    	if($stmt = mysqli_prepare($link, $sql)){
+    		mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+    		$param_username = $username;
+
+    		if(mysqli_stmt_execute($stmt)){
+    			mysqli_stmt_store_result($stmt);
+
+    			if(mysqli_stmt_num_rows($stmt) == 1){     
+    				mysqli_stmt_bind_result($stmt, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                        	session_start();
+                            $_SESSION['username'] = $username;      
+                            header("location: welcome.php"); //CHANGE THIS TO THE ADMIN PAGE FOR VIEWING THE TABLE
+                         } else{
+                         	 $password_err = 'The password you entered was not valid.';
+                         }
+                    }
+                } else {
+                	$username_err = 'No account found with that username.';
+                }
+            } else {
+            	echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+
+    	mysqli_stmt_close($stmt);
+    }
+
+    mysqli_close($link);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>Login</title>
+
+<head>
+   <meta charset="utf-8">
+
+	<title>Home</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+	
 		<link rel="stylesheet" href="css/styles.css">
 		<link rel="stylesheet" href="css/nav.css">
 		<!-- Insert Fonts Here -->
 	</head>
-
+	
+		
 	<body>
+
+<header>
+	  
+		<?php include "includes/nav.inc" ?>
+	</header>
+
+<hr>
 		<!-- Login Stuff -->
 		<p>New User? Sign Up Here</p>
 
@@ -17,7 +88,7 @@
 		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 			<div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
-                <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
                 <span class="help-block"><?php echo $username_err; ?></span>
             </div>    
             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
